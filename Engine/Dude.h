2d1d4c6885f3f0ec2f350f907a3c6a71041823e7
dieWithYourBootsON <kumarrahul007@gmail.com>
2021-04-03 21:24:14 +0530

    -Normalized Vec2 implemented -Mouse movement added

--- Engine/Dude.h
+++ Engine/Dude.h
@@ -2,6 +2,7 @@
 
 #include "Graphics.h"
 #include "Keyboard.h"
+#include "Mouse.h"
 #include "Vec2.h"
 
 class Dude
@@ -10,11 +11,13 @@ public:
 	void ClampToScreen();
 	void Draw( Graphics& gfx ) const;
 	void Update( const Keyboard& kbd,float dt );
+	void Update(const Mouse& mouse, float dt);
 	Vec2 GetPos() const;
 	float GetWidth() const;
 	float GetHeight() const;
 private:
 	Vec2 pos = Vec2( 400.0f, 300.0f );
+	Vec2 mousePos = Vec2(400.0f, 300.0f);
 	static constexpr float speed = 1.0f * 60.0f;
 	static constexpr float width = 20.0f;
 	static constexpr float height = 20.0f;
